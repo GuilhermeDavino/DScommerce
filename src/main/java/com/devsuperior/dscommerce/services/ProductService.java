c4a0d1260ece64425c0b5b2b779eb4401da66a41
pc-roze <guilherme.1009@hotmail.com>
2023-12-30 18:35:14 -0300

    PUT product and DELETE produts

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -13,32 +13,53 @@ import com.devsuperior.dscommerce.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
-	
+
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Product product = repository.findById(id).get();
 		return modelMapper.map(product, ProductDTO.class);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> result = repository.findAll(pageable);
-		return result.map(x -> modelMapper.map(x , ProductDTO.class));
+		return result.map(x -> modelMapper.map(x, ProductDTO.class));
 	}
-	
+
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
 		modelMapper.map(dto, entity);
 		entity = repository.save(entity);
 		return modelMapper.map(entity, ProductDTO.class);
-		
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = repository.getReferenceById(id);
+		DTOtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return modelMapper.map(entity, ProductDTO.class);
+
 	}
 	
+	
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	private void DTOtoEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+	}
+
 }
